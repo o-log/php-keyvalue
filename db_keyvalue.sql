ba029949df84b6786656477456db4bfd1d3bcda9
Maxim Kozlov <650465@gmail.com>
2017-02-09 12:18:42 +0300

    Update db_keyvalue.sql

--- db_keyvalue.sql
+++ db_keyvalue.sql
@@ -5,4 +5,5 @@ array (
 'alter table olog_keyvalue_keyvalue add unique key UK_name_722823 (name)  /* rand586465 */;',
 'alter table olog_keyvalue_keyvalue CHANGE COLUMN `value` `value` text /* rand32w984122123232791 */',
 'insert into olog_auth_permission (title) values ("PERMISSION_KEYVALUE_MANAGE") /* rand26524034571q1 */;',
+'alter table olog_keyvalue_keyvalue CHANGE COLUMN `value` `value` mediumtext /* rand2123232791 */',
 )
